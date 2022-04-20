8717332a3ad15b639fdc6cfe01301398cd5d7318
Egor <savinovego05@gmail.com>
2022-04-20 19:57:06 +0300

    initial commit

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,18 @@
 from sqlalchemy.sql.expression import select
 from data_base import User, Session
+import hashlib
+
+
+def hash_password(password: str, salt=""):
+    secret_bytes = password.encode()
+    salt_bytes = salt.encode()
+    return hashlib.pbkdf2_hmac("sha256", secret_bytes, salt_bytes, 100000).hex()
 
 
 def add_user(user_name, pass_hash, first_name):
     with Session.begin() as session:
         if session.execute(select(User).where(User.user_name == user_name)).scalar():
-            raise Exception(f"User with user_name {user_name}")
+            raise Exception('username is unique')
 
         new_user = User(
             user_name=user_name,
@@ -23,15 +30,53 @@ def add_user(user_name, pass_hash, first_name):
         )
 
 
-# def get_user(user_name, , User, ):
-#     with Session.begin() as session:
-#         users = session.execute(select(User)).scalar().all()
-#         return [
-#             dict(
-#                 user_name=fixed_username,
-#                 pass_hash=users.pass_hash,
-#                 id=users.id,
-#                 first_name=users.first_name
-#             )
-#             for i in users
-#         ]
+def get_user(user_name):
+    with Session.begin() as session:
+        user = session.execute(select(User).where(User.user_name == user_name)).scalar()
+
+        return dict(
+            user_name=user.user_name,
+            pass_hash=user.pass_hash,
+            id=user.id,
+            first_name=user.firstname
+        )
+
+
+def get_users():
+    with Session.begin() as session:
+        users = session.execute(select(User)).scalars().all()
+        return [dict(user_name=u.user_name,
+                     pass_hash=u.pass_hash,
+                     id=u.id,
+                     first_name=u.firstname
+                     ) for u in users]
+
+
+def register(user_name, password):
+    with Session.begin() as session:
+        if session.execute(select(User).where(User.user_name == user_name)).scalar():
+            raise Exception('username is unique')
+
+        new_user = User(
+            user_name=user_name,
+            pass_hash=hash_password(password)
+        )
+
+        session.add(new_user)
+        session.flush()
+
+        return dict(
+            user_name=new_user.user_name,
+            pass_hash=new_user.pass_hash,
+        )
+
+
+def login(user_name, password):
+    with Session.begin() as session:
+        user = session.execute(
+            select(User).where(User.user_name == user_name).where(User.pass_hash == hash_password(password))).scalar()
+        if user is None:
+            raise Exception
+        return dict( user_name=user.user_name,
+                     pass_hash=user.pass_hash,
+        )
--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 import data_base
 from api import add_user
-
+from api import get_user
+from api import get_users, hash_password, register, login
 if __name__ == "__main__":
     data_base.init()
-    usr = add_user(user_name="Ivan",
-                   pass_hash='11221',
-                   first_name="Ivan")
-    print(usr)
+    # usr = add_user(user_name="Ivan10",
+    #                pass_hash='11221',
+    #                first_name="Ivan")
+    # print(usr)
+    #
+    # data_base.init()
+    # user = get_user(user_name="Ivan")
+    #
+    #
+    # print(user)
+
+    # users = get_users()
+
+    # print(users)
+
+
+
+    # print(hash_password("Hello"), hash_password("Hello", "1234"))
+
+    # reg = register(user_name="Ivan11",
+    #                password="11221",
+    #                )
+    # print(reg)
+    log = login(user_name="Ivan11",
+                password="1121")
+    print(log)
